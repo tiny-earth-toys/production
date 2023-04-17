1b1575c0f886533f71db374454200b6f7950074c
Logan Atkinson <83839762+thenlogansays@users.noreply.github.com>
2023-04-17 14:08:10 -0400

    set slidesPerPage to 1 in global.js slider component

--- assets/global.js
+++ assets/global.js
@@ -545,8 +545,8 @@ class SliderComponent extends HTMLElement {
     this.sliderItemsToShow = Array.from(this.sliderItems).filter(element => element.clientWidth > 0);
     if (this.sliderItemsToShow.length < 2) return;
     this.sliderItemOffset = this.sliderItemsToShow[1].offsetLeft - this.sliderItemsToShow[0].offsetLeft;
-    this.slidesPerPage = Math.floor((this.slider.clientWidth - this.sliderItemsToShow[0].offsetLeft) / this.sliderItemOffset);
-    this.totalPages = this.sliderItemsToShow.length - this.slidesPerPage + 1;
+    this.slidesPerPage = 1;
+    this.totalPages = this.sliderItemsToShow.length - this.slidesPerPage +1;
     this.update();
   }
   resetPages() {
